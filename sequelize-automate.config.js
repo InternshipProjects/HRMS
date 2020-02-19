91d9e5094d0a0a67a8014622dfb12a872a8a5e79
Dhanalakshmi Narala <dhanalakshmi.narala@gmail.com>
2020-02-19 13:30:41 +0530

    Add seqelize-automate config file

---
+++ sequelize-automate.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  dbOptions: {
+    database: 'hrms',
+    username: 'dhana',
+    password: 'dhana123',
+    dialect: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    logging: false
+  },
+  options: {
+    type: 'js',
+    dir: 'models'
+  }
+};
