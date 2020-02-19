5c40ec38a1d22738ca9618bdcac209634aba77b7
Dhanalakshmi Narala <dhanalakshmi.narala@gmail.com>
2020-02-19 13:24:49 +0530

    Refactor xlsx parser

--- xlsx/parse_xlsx.js
+++ xlsx/parse_xlsx.js
@@ -1,8 +1,8 @@
 const XLSX = require('xlsx');
 
-const convertToJSON = (fileName, sheetName) => {
+const convertXlsxToJson = (fileName, sheetName) => {
   const workbook = XLSX.readFile(fileName);
   return XLSX.utils.sheet_to_json(workbook.Sheets[sheetName]);
 };
 
-module.exports = convertToJSON;
+module.exports = convertXlsxToJson;
