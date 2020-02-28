e619a30e6769ce2cd43580199cf1b4b2ab00f432
Dhanalakshmi Narala <dhanalakshmi.narala@gmail.com>
2020-02-28 18:04:40 +0530

    Add helper methods for testing

--- test/helpers.js
+++ test/helpers.js
@@ -1,7 +1,40 @@
+const expect = require('chai').expect;
 const sequelize = require('../src/utils/connect_sequelize');
+const EmployeeModel = require('../models/employee')(sequelize);
+const ProjectModel = require('../models/project')(sequelize);
 
 const truncateTable = async tableName => {
   await sequelize.query(`truncate ${tableName} cascade`);
 };
 
-module.exports = { truncateTable };
+const insertEmployee = async employee => {
+  await EmployeeModel.create(employee);
+  const employees = await EmployeeModel.findAll({
+    raw: true,
+    where: { emp_id: employee.emp_id }
+  });
+  expect(employees).to.have.lengthOf(1);
+  const employeeInfo = employees[0];
+  expect(employeeInfo).to.have.property('emp_id', employee.emp_id);
+  expect(employeeInfo).to.have.property('name', employee.name);
+  expect(employeeInfo).to.have.property('email', employee.email);
+  expect(employeeInfo).to.have.property('address', employee.address);
+  expect(employeeInfo).to.have.property('phone_no', employee.phone_no);
+  return employeeInfo;
+};
+
+const insertProject = async project => {
+  await ProjectModel.create(project);
+  const projects = await ProjectModel.findAll({
+    raw: true,
+    where: { name: project.name }
+  });
+  expect(projects).to.have.lengthOf(1);
+  const projectInfo = projects[0];
+  expect(projectInfo).to.have.property('name', project.name);
+  expect(projectInfo).to.have.property('start_date', project.start_date);
+  expect(projectInfo).to.have.property('end_date', projectInfo.end_date);
+  return projectInfo;
+};
+
+module.exports = { truncateTable, insertEmployee, insertProject };
