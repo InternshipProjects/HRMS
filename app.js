22771a6de051b9b2fbb94e645c0eff24917f534a
Dhanalakshmi Narala <dhanalakshmi.narala@gmail.com>
2020-03-01 17:54:56 +0530

    Add swagger-options for apis documentation

--- app.js
+++ app.js
@@ -2,6 +2,22 @@ const express = require('express');
 const app = express();
 const morgon = require('morgan');
 const bodyParser = require('body-parser');
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+
+const swaggerOptions = {
+  swaggerDefinition: {
+    info: {
+      title: 'HRMS API',
+      description: 'Human Resource Management System for a company'
+    },
+    servers: ['http://localhost:5000']
+  },
+  apis: ['./src/api/routes/*.js']
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 app.use(morgon('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
