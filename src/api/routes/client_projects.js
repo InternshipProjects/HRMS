3ab6b6f6f92c02a2550fd1b6ec9dc3a9d2671a6c
Dhanalakshmi Narala <dhanalakshmi.narala@gmail.com>
2020-03-02 08:33:58 +0530

    Add client_projects apis

---
+++ src/api/routes/client_projects.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const router = express.Router();
+const ClientProjectController = require('../../controller/project/client_projects');
+
+router.post('/', async (req, res, next) => {
+  const params = {
+    client_name: req.body.client_name,
+    project_name: req.body.project_name
+  };
+  try {
+    await ClientProjectController.insert(params);
+    return res.status(200).json({
+      message: 'Post method on client_projects',
+      params
+    });
+  } catch (error) {
+    return res.status(400).json({
+      error: error.message
+    });
+  }
+});
+
+router.get('/', async (req, res, next) => {
+  const params = {
+    client_name: req.body.client_name
+  };
+  try {
+    const projects = await ClientProjectController.select(params);
+    return res.status(200).json({
+      message: 'Get method on client_projects',
+      results: projects,
+      params
+    });
+  } catch (error) {
+    return res.status(400).json({
+      error: error.message
+    });
+  }
+});
+
+router.delete('/', async (req, res, next) => {
+  const params = {
+    client_name: req.body.client_name,
+    project_name: req.body.project_name
+  };
+  try {
+    await ClientProjectController.delete(params);
+    return res.status(200).json({
+      message: 'Delete method on client_projects',
+      params
+    });
+  } catch (error) {
+    return res.status(400).json({
+      error: error.message
+    });
+  }
+});
+
+module.exports = router;
---
+++ src/controller/project/client_projects.js
@@ -0,0 +1,29 @@
+const Client = require('../client');
+const Project = require('./project');
+const sequelize = require('../../utils/connect_sequelize');
+const { QueryTypes } = require('sequelize');
+
+class ClientProjects {
+  async insert(params) {
+    const clientInfo = await Client.select({ name: params.client_name });
+    await Project.update({
+      name: params.project_name,
+      client_id: clientInfo[0].id
+    });
+  }
+
+  async select(params) {
+    const clientInfo = await Client.select({ name: params.client_name });
+    return Project.select({ client_id: clientInfo[0].id });
+  }
+
+  async delete(params) {
+    const clientInfo = await Client.select({ name: params.client_name });
+    await Project.delete({
+      client_id: clientInfo[0].id,
+      name: params.project_name
+    });
+  }
+}
+
+module.exports = new ClientProjects();
