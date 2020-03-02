113bf07def3bd6475bae7a1b77a88aefc9db3673
Dhanalakshmi Narala <dhanalakshmi.narala@gmail.com>
2020-03-02 11:12:42 +0530

    Remove data/ index.js utils/json_handler, utils/parse_xlsx utils/queries_handler

--- src/utils/parse_xlsx.js
+++
@@ -1,8 +0,0 @@
-const XLSX = require('xlsx');
-
-const convertXlsxToJson = (fileName, sheetName) => {
-  const workbook = XLSX.readFile(fileName);
-  return XLSX.utils.sheet_to_json(workbook.Sheets[sheetName]);
-};
-
-module.exports = convertXlsxToJson;
