0031830526357069f8660dad6b9b3519b77c18a2
Dhanalakshmi Narala <dhanalakshmi.narala@gmail.com>
2020-03-03 20:46:48 +0530

    Add CORS errors handling

--- src/app.js
+++ src/app.js
@@ -15,6 +15,20 @@ const employeesAvailabilityRoutes = require('./api/routes/employees_availability
 app.use(morgon('dev'));
 app.use(express.json());
 
+// Handling CORS errors
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'POST, PATCH, DELETE, GET');
+    return res.status(200).json({});
+  }
+  next();
+});
+
 app.use('/company', companyRoutes);
 app.use('/employee', employeeRoutes);
 app.use('/employee_skills', employeeSkillsRoutes);
