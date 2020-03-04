874f142d417375c7430a814b504cfcb0380720c6
Dhanalakshmi Narala <dhanalakshmi.narala@gmail.com>
2020-03-04 22:41:53 +0530

    Refactor company_employees controller unit tests

--- test/company_employees.js
+++ test/company_employees.js
@@ -19,19 +19,8 @@ describe('CompanyEmployees table', () => {
 
   describe('insert', () => {
     it('should insert employee1 into company1', async () => {
-      await CompanyModel.create(companies[0]);
-      const companyResults = await CompanyModel.findAll({
-        raw: true,
-        where: { registration_no: companies[0].registration_no }
-      });
-      Helper.compareCompanyResults(companyResults, companies[0]);
-
-      await EmployeeModel.create(employees[0]);
-      const employeeResults = await EmployeeModel.findAll({
-        raw: true,
-        where: { emp_id: employees[0].emp_id }
-      });
-      Helper.compareEmployeeResults(employeeResults, employees[0]);
+      const companyInfo = await Helper.insertCompany(companies[0]);
+      const employeeInfo = await Helper.insertEmployee(employees[0]);
 
       await CompanyEmployees.insert({
         registration_no: companies[0].registration_no,
@@ -40,29 +29,14 @@ describe('CompanyEmployees table', () => {
 
       const results = await CompanyEmployeesModel.findAll({
         raw: true,
-        where: { company_id: companyResults[0].id }
+        where: { company_id: companyInfo.id }
       });
-
-      expect(results).to.have.lengthOf(1);
-      expect(results[0]).to.have.property('id');
-      expect(results[0]).to.have.property('company_id', companyResults[0].id);
-      expect(results[0]).to.have.property('employee_id', employeeResults[0].id);
+      compareCompanyEmployeesResults(results, companyInfo.id, employeeInfo.id);
     });
 
     it('should insert employee2 into company2', async () => {
-      await CompanyModel.create(companies[1]);
-      const companyResults = await CompanyModel.findAll({
-        raw: true,
-        where: { registration_no: companies[1].registration_no }
-      });
-      Helper.compareCompanyResults(companyResults, companies[1]);
-
-      await EmployeeModel.create(employees[1]);
-      const employeeResults = await EmployeeModel.findAll({
-        raw: true,
-        where: { emp_id: employees[1].emp_id }
-      });
-      Helper.compareEmployeeResults(employeeResults, employees[1]);
+      const companyInfo = await Helper.insertCompany(companies[1]);
+      const employeeInfo = await Helper.insertEmployee(employees[1]);
 
       await CompanyEmployees.insert({
         registration_no: companies[1].registration_no,
@@ -71,73 +45,38 @@ describe('CompanyEmployees table', () => {
 
       const results = await CompanyEmployeesModel.findAll({
         raw: true,
-        where: { company_id: companyResults[0].id }
+        where: { company_id: companyInfo.id }
       });
 
-      expect(results).to.have.lengthOf(1);
-      expect(results[0]).to.have.property('id');
-      expect(results[0]).to.have.property('company_id', companyResults[0].id);
-      expect(results[0]).to.have.property('employee_id', employeeResults[0].id);
+      compareCompanyEmployeesResults(results, companyInfo.id, employeeInfo.id);
     });
   });
 
   describe('select', () => {
     it('should select company1 employees details', async () => {
-      await CompanyModel.create(companies[0]);
-      const companyResults = await CompanyModel.findAll({
-        raw: true,
-        where: { registration_no: companies[0].registration_no }
-      });
-      Helper.compareCompanyResults(companyResults, companies[0]);
+      const companyInfo = await Helper.insertCompany(companies[0]);
 
-      await insertEmployeeInCompany(employees[0], companyResults[0].id);
-      await insertEmployeeInCompany(employees[1], companyResults[0].id);
+      await insertEmployeeInCompany(employees[0], companyInfo.id);
+      await insertEmployeeInCompany(employees[1], companyInfo.id);
 
       const employeesInfo = await CompanyEmployees.select({
         registration_no: companies[0].registration_no
       });
-      expect(employeesInfo).to.have.lengthOf(2);
-      employeesInfo.forEach((employee, index) => {
-        expect(employee).to.have.property(
-          'employee_model.emp_id',
-          employees[index].emp_id
-        );
-        expect(employee).to.have.property(
-          'employee_model.name',
-          employees[index].name
-        );
-        expect(employee).to.have.property(
-          'employee_model.email',
-          employees[index].email
-        );
-        expect(employee).to.have.property(
-          'employee_model.address',
-          employees[index].address
-        );
-        expect(employee).to.have.property(
-          'employee_model.phone_no',
-          employees[index].phone_no
-        );
-      });
+      compareEmployeesInfo(employeesInfo, 2);
     });
   });
 
   describe('delete', () => {
     it('should delete employee1 from company1', async () => {
-      await CompanyModel.create(companies[0]);
-      const companyResults = await CompanyModel.findAll({
-        raw: true,
-        where: { registration_no: companies[0].registration_no }
-      });
-      Helper.compareCompanyResults(companyResults, companies[0]);
+      const companyInfo = await Helper.insertCompany(companies[0]);
 
       const employee1Id = await insertEmployeeInCompany(
         employees[0],
-        companyResults[0].id
+        companyInfo.id
       );
       const employee2Id = await insertEmployeeInCompany(
         employees[1],
-        companyResults[0].id
+        companyInfo.id
       );
 
       await CompanyEmployees.delete({
@@ -148,27 +87,52 @@ describe('CompanyEmployees table', () => {
       const results = await CompanyEmployeesModel.findAll({
         raw: true,
         where: {
-          company_id: companyResults[0].id
+          company_id: companyInfo.id
         }
       });
-      expect(results).to.have.lengthOf(1);
-      expect(results[0]).to.have.property('company_id', companyResults[0].id);
-      expect(results[0]).to.have.property('employee_id', employee2Id);
+      compareCompanyEmployeesResults(results, companyInfo.id, employee2Id);
     });
   });
 
   const insertEmployeeInCompany = async (employee, companyId) => {
-    await EmployeeModel.create(employee);
-    const employeeResults = await EmployeeModel.findAll({
-      raw: true,
-      where: { emp_id: employee.emp_id }
-    });
-    Helper.compareEmployeeResults(employeeResults, employee);
-
+    const employeeInfo = await Helper.insertEmployee(employee);
     await CompanyEmployeesModel.create({
-      employee_id: employeeResults[0].id,
+      employee_id: employeeInfo.id,
       company_id: companyId
     });
-    return employeeResults[0].id;
+    return employeeInfo.id;
+  };
+
+  const compareCompanyEmployeesResults = (results, companyId, employeeId) => {
+    expect(results).to.have.lengthOf(1);
+    expect(results[0]).to.have.property('id');
+    expect(results[0]).to.have.property('company_id', companyId);
+    expect(results[0]).to.have.property('employee_id', employeeId);
+  };
+
+  const compareEmployeesInfo = (employeesInfo, employeesCount) => {
+    expect(employeesInfo).to.have.lengthOf(employeesCount);
+    employeesInfo.forEach((employee, index) => {
+      expect(employee).to.have.property(
+        'employee_model.emp_id',
+        employees[index].emp_id
+      );
+      expect(employee).to.have.property(
+        'employee_model.name',
+        employees[index].name
+      );
+      expect(employee).to.have.property(
+        'employee_model.email',
+        employees[index].email
+      );
+      expect(employee).to.have.property(
+        'employee_model.address',
+        employees[index].address
+      );
+      expect(employee).to.have.property(
+        'employee_model.phone_no',
+        employees[index].phone_no
+      );
+    });
   };
 });
