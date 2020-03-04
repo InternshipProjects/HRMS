1749d799b30b0866a5d46db879661239e821cd78
Dhanalakshmi Narala <dhanalakshmi.narala@gmail.com>
2020-03-04 11:54:47 +0530

    Refactor user controller unit tests

--- test/users.js
+++ test/users.js
@@ -1,21 +1,23 @@
 const expect = require('chai').expect;
 const bcrypt = require('bcrypt');
 
+const User = require('../src/controller/user');
 const sequelize = require('../src/utils/connect_sequelize');
 const UserModel = require('../src/models/users')(sequelize);
-const User = require('../src/controller/user');
-const { truncateTable } = require('./helpers');
+const Helper = require('./helper');
 
 describe('User Table', () => {
   const users = [
     {
       name: 'dhanalakshmi',
+      user_name: 'dhana',
       password: 'dhana123',
       email: 'dhanalakshmi.narala@gmail.com',
       phone_no: '9553113446'
     },
     {
       name: 'sailaja',
+      user_name: 'sailu',
       password: 'sailu123',
       email: 'sailu@gmail.com',
       phone_no: '1234567890'
@@ -23,7 +25,7 @@ describe('User Table', () => {
   ];
 
   beforeEach(async () => {
-    await truncateTable('users');
+    await Helper.truncateTable('users');
   });
 
   describe('insert', () => {
@@ -31,7 +33,7 @@ describe('User Table', () => {
       await User.insert(users[0]);
       const results = await UserModel.findAll({
         raw: true,
-        where: { name: users[0].name }
+        where: { user_name: users[0].user_name }
       });
       await compareUserDeatils(results, users[0]);
     });
@@ -40,7 +42,7 @@ describe('User Table', () => {
       await User.insert(users[1]);
       const results = await UserModel.findAll({
         raw: true,
-        where: { name: users[1].name }
+        where: { user_name: users[1].user_name }
       });
       await compareUserDeatils(results, users[1]);
     });
@@ -49,13 +51,13 @@ describe('User Table', () => {
   describe('select', () => {
     it('should select user1 details', async () => {
       await UserModel.create(users[0]);
-      const results = await User.select({ name: users[0].name });
+      const results = await User.select({ user_name: users[0].user_name });
       await compareUserDeatils(results, users[0]);
     });
 
     it('should select user2 details', async () => {
       await UserModel.create(users[1]);
-      const results = await User.select({ name: users[1].name });
+      const results = await User.select({ user_name: users[1].user_name });
       await compareUserDeatils(results, users[1]);
     });
   });
@@ -64,11 +66,11 @@ describe('User Table', () => {
     it('should update email of user1', async () => {
       await UserModel.create(users[0]);
       const newEmail = 'dhanalakshmi@gmail.com';
-      await User.update({ name: users[0].name, email: newEmail });
+      await User.update({ user_name: users[0].user_name, email: newEmail });
 
       const results = await UserModel.findAll({
         raw: true,
-        where: { name: users[0].name }
+        where: { user_name: users[0].user_name }
       });
       await compareUserDeatils(results, users[0]);
     });
@@ -76,11 +78,11 @@ describe('User Table', () => {
     it('should update email of user2', async () => {
       await UserModel.create(users[1]);
       const newEmail = 'sailaja@gmail.com';
-      await User.update({ name: users[1].name, email: newEmail });
+      await User.update({ user_name: users[1].user_name, email: newEmail });
 
       const results = await UserModel.findAll({
         raw: true,
-        where: { name: users[1].name }
+        where: { user_name: users[1].user_name }
       });
       await compareUserDeatils(results, users[1]);
     });
@@ -89,20 +91,20 @@ describe('User Table', () => {
   describe('delete', () => {
     it('should delete user1', async () => {
       await UserModel.create(users[0]);
-      await User.delete({ name: users[0].name });
+      await User.delete({ user_name: users[0].user_name });
       const results = await UserModel.findAll({
         raw: true,
-        where: { name: users[0].name }
+        where: { user_name: users[0].user_name }
       });
       expect(results).to.have.lengthOf(0);
     });
 
     it('should delete user2', async () => {
       await UserModel.create(users[1]);
-      await User.delete({ name: users[1].name });
+      await User.delete({ user_name: users[1].user_name });
       const results = await UserModel.findAll({
         raw: true,
-        where: { name: users[1].name }
+        where: { user_name: users[1].user_name }
       });
       expect(results).to.have.lengthOf(0);
     });
@@ -111,9 +113,10 @@ describe('User Table', () => {
   const compareUserDeatils = async (results, user) => {
     expect(results).to.have.lengthOf(1);
     const userInfo = results[0];
-    expect(userInfo).to.have.property('name', user.name);
+    expect(userInfo).to.have.property('user_name', user.user_name);
     expect(userInfo).to.have.property('password');
     expect(await bcrypt.compare(user.password, userInfo.password));
+    expect(userInfo).to.have.property('name', userInfo.name);
     expect(userInfo).to.have.property('email', userInfo.email);
     expect(userInfo).to.have.property('phone_no', userInfo.phone_no);
   };
