ae6c86716b53fbb68bf282da4735c98a40e68634
Dhanalakshmi Narala <dhanalakshmi.narala@gmail.com>
2020-03-04 23:28:59 +0530

    Refactor project unit tests

--- test/project.js
+++ test/project.js
@@ -2,126 +2,109 @@ const expect = require('chai').expect;
 
 const sequelize = require('../src/utils/connect_sequelize');
 const ProjectModel = require('../src/models/project')(sequelize);
-const Project = require('../src/controller/project/project');
-const { truncateTable } = require('./helpers');
+const Project = require('../src/controller/project');
+const Helper = require('./helper');
+const { projectes } = require('./data');
 
 describe('Project Table', () => {
-  const project1 = {
-    name: 'todos',
-    start_date: '2020-02-27',
-    end_date: '2020-03-27'
-  };
-  const project2 = {
-    name: 'tic-toc-toe',
-    start_date: '2020-04-01',
-    end_date: '2020-05-01'
-  };
-
   beforeEach(async () => {
-    await truncateTable('project');
+    await Helper.truncateTable('project');
   });
 
   describe('insert', () => {
     it('should insert project1', async () => {
-      await Project.insert(project1);
+      await Project.insert(projectes[0]);
       const results = await ProjectModel.findAll({
         raw: true,
-        where: { name: project1.name }
+        where: { name: projectes[0].name }
       });
-      expect(results).to.have.lengthOf(1);
-      const projectInfo = results[0];
-      expect(projectInfo).to.have.property('name', project1.name);
-      expect(projectInfo).to.have.property('start_date', project1.start_date);
-      expect(projectInfo).to.have.property('end_date', project1.end_date);
+      compareProjectResults(results, projectes[0]);
     });
 
     it('should insert project2', async () => {
-      await Project.insert(project2);
+      await Project.insert(projectes[1]);
       const results = await ProjectModel.findAll({
         raw: true,
-        where: { name: project2.name }
+        where: { name: projectes[1].name }
       });
-      expect(results).to.have.lengthOf(1);
-      const projectInfo = results[0];
-      expect(projectInfo).to.have.property('name', project2.name);
-      expect(projectInfo).to.have.property('start_date', project2.start_date);
-      expect(projectInfo).to.have.property('end_date', project2.end_date);
+      compareProjectResults(results, projectes[1]);
     });
   });
 
   describe('select', () => {
     it('should select project1 details', async () => {
-      await ProjectModel.create(project1);
-      const results = await Project.select({ name: project1.name });
-      expect(results).to.have.lengthOf(1);
-      const projectInfo = results[0];
-      expect(projectInfo).to.have.property('name', project1.name);
-      expect(projectInfo).to.have.property('start_date', project1.start_date);
-      expect(projectInfo).to.have.property('end_date', project1.end_date);
+      await ProjectModel.create(projectes[0]);
+      const results = await Project.select({ name: projectes[0].name });
+      compareProjectResults(results, projectes[0]);
     });
 
     it('should select project2 details', async () => {
-      await ProjectModel.create(project2);
-      const results = await Project.select({ name: project2.name });
-      expect(results).to.have.lengthOf(1);
-      const projectInfo = results[0];
-      expect(projectInfo).to.have.property('name', project2.name);
-      expect(projectInfo).to.have.property('start_date', project2.start_date);
-      expect(projectInfo).to.have.property('end_date', project2.end_date);
+      await ProjectModel.create(projectes[1]);
+      const results = await Project.select({ name: projectes[1].name });
+      compareProjectResults(results, projectes[1]);
     });
   });
 
   describe('update', () => {
     it('should update project1 details', async () => {
-      await ProjectModel.create(project1);
+      await ProjectModel.create(projectes[0]);
       const newStartDate = '2020-04-01';
-      await Project.update({ name: project1.name, start_date: newStartDate });
+      await Project.update({
+        name: projectes[0].name,
+        start_date: newStartDate
+      });
       const results = await ProjectModel.findAll({
         raw: true,
-        where: { name: project1.name }
+        where: { name: projectes[0].name }
       });
-      expect(results).to.have.lengthOf(1);
-      const projectInfo = results[0];
-      expect(projectInfo).to.have.property('name', project1.name);
-      expect(projectInfo).to.have.property('start_date', newStartDate);
-      expect(projectInfo).to.have.property('end_date', project1.end_date);
+
+      const updatedProjectInfo = Object.assign(projectes[0], {});
+      updatedProjectInfo.start_date = newStartDate;
+      compareProjectResults(results, updatedProjectInfo);
     });
 
     it('should update project2 details', async () => {
-      await ProjectModel.create(project2);
+      await ProjectModel.create(projectes[1]);
       const newEndDate = '2020-08-17';
-      await Project.update({ name: project2.name, end_date: newEndDate });
+      await Project.update({ name: projectes[1].name, end_date: newEndDate });
       const results = await ProjectModel.findAll({
         raw: true,
-        where: { name: project2.name }
+        where: { name: projectes[1].name }
       });
-      expect(results).to.have.lengthOf(1);
-      const projectInfo = results[0];
-      expect(projectInfo).to.have.property('name', project2.name);
-      expect(projectInfo).to.have.property('start_date', project2.start_date);
-      expect(projectInfo).to.have.property('end_date', newEndDate);
+
+      const updatedProjectInfo = Object.assign(projectes[1], {});
+      updatedProjectInfo.end_date = newEndDate;
+      compareProjectResults(results, updatedProjectInfo);
     });
   });
 
   describe('delete', () => {
     it('should delete project1', async () => {
-      await ProjectModel.create(project1);
-      await Project.delete({ name: project1.name });
+      await ProjectModel.create(projectes[0]);
+      await Project.delete({ name: projectes[0].name });
       const results = await ProjectModel.findAll({
         raw: true,
-        where: { name: project1.name }
+        where: { name: projectes[0].name }
       });
       expect(results).to.have.lengthOf(0);
     });
 
     it('should delete project2', async () => {
-      await ProjectModel.create(project2);
-      await Project.delete({ name: project2.name });
+      await ProjectModel.create(projectes[1]);
+      await Project.delete({ name: projectes[1].name });
       const results = await ProjectModel.findAll({
         raw: true,
-        where: { name: project2.name }
+        where: { name: projectes[1].name }
       });
       expect(results).to.have.lengthOf(0);
     });
   });
+
+  const compareProjectResults = (results, project) => {
+    expect(results).to.have.lengthOf(1);
+    const projectInfo = results[0];
+    expect(projectInfo).to.have.property('name', project.name);
+    expect(projectInfo).to.have.property('start_date', project.start_date);
+    expect(projectInfo).to.have.property('end_date', project.end_date);
+  };
 });
