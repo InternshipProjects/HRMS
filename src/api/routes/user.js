17edf96ef1a26ce565f8119e868ce89df16e7c47
Dhanalakshmi Narala <dhanalakshmi.narala@gmail.com>
2020-03-05 12:38:27 +0530

    Add confirm_password field to user api

--- src/api/routes/user.js
+++ src/api/routes/user.js
@@ -4,17 +4,24 @@ const UserController = require('../../controller/user');
 
 router.post('/', async (req, res, next) => {
   const user = {
+    user_name: req.body.user_name,
     name: req.body.name,
     password: req.body.password,
+    confirm_password: req.body.confirm_password,
     email: req.body.email,
     phone_no: req.body.phone_no
   };
   try {
-    await UserController.insert(user);
-    return res.status(200).json({
-      message: 'New user created',
-      params: user
-    });
+    if (user.password === user.confirm_password) {
+      await UserController.insert(user);
+      return res.status(200).json({
+        message: 'New user created',
+        params: user
+      });
+    }
+    const error = new Error('Confirm password and password should be same');
+    error.status = 400;
+    next(error);
   } catch (error) {
     console.error(error);
     error.status = 400;
@@ -24,7 +31,7 @@ router.post('/', async (req, res, next) => {
 
 router.get('/', async (req, res, next) => {
   const params = {
-    name: req.body.name
+    user_name: req.body.user_name
   };
   try {
     const results = await UserController.select(params);
@@ -41,6 +48,7 @@ router.get('/', async (req, res, next) => {
 
 router.patch('/', async (req, res, next) => {
   const user = {
+    user_name: req.body.user_name,
     name: req.body.name,
     password: req.body.password,
     email: req.body.email,
@@ -60,7 +68,7 @@ router.patch('/', async (req, res, next) => {
 
 router.delete('/', async (req, res, next) => {
   const params = {
-    name: req.body.name
+    user_name: req.body.user_name
   };
   try {
     await UserController.delete(params);
